8ddf98e7a74cffc4e44588e4dee8f013c889cc84
Satyam Seth <satyam1998.1998@gmail.com>
2025-05-17 00:07:17 +0530

    Add Order_by and Group_by

---
+++ Geekyshows/Section-10.Working with Databases Using SQLAlchemy Core/7.Order_by and Group_by/ch39/main.py
@@ -0,0 +1,21 @@
+from tables import create_tables
+from services import *
+
+# Create Tables
+create_tables()
+
+# Create Data
+# create_user("sonam", "sonam@example.com")
+# create_user("raj", "raj@example.com")
+# create_post(1, "Hello World", "This is Sonam's first post")
+# create_post(1, "Bye World", "This is Sonam's second post")
+# create_post(1, "No World", "This is Sonam's third post")
+# create_post(2, "Raj's Post 1", "Hi from Raj! 1")
+# create_post(2, "Raj's Post 2", "Hi from Raj! 2")
+
+# Order By
+# print(get_users_ordered_by_name())
+# print(get_posts_latest_first())
+
+# Group By
+# print(get_post_count_per_user())
