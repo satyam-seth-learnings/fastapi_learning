f1065faa0111e688623ab8da4924284263dcc8cf
Satyam Seth <satyam1998.1998@gmail.com>
2025-06-17 21:47:19 +0530

    Add create model

---
+++ Geekyshows/Section-17.FastAPI with SQL Model/2.Create Model/ch70/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.user.models import User
+from app.product.models import Product
+from app.db.config import create_tables
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  create_tables()
+  yield
+
+app = FastAPI(lifespan=lifespan)
