e07d750659b4d79c7c8788a1b681bb058e7b9681
Satyam Seth <satyam1998.1998@gmail.com>
2026-05-23 21:31:31 +0530

    React JS Integration

---
+++ Geekyshows/Section-25.Frontend Integrations/4.React JS Integration/ch100/reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductList from './components/ProductList';
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <ProductList />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Geekyshows/Section-25.Frontend Integrations/4.React JS Integration/ch100/reactjs/src/components/ProductList.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import ProductForm from './ProductForm';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const res = await fetch('http://localhost:8000/products');
+    const data = await res.json();
+    setProducts(data);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Product List</h1>
+      <ProductForm onProductAdded={(product) => setProducts([...products, product])} />
+      <hr />
+       <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            <strong>{p.title}</strong>: {p.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ProductList
