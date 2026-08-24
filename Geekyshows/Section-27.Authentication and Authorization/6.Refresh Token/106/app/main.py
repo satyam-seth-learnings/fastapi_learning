2836ff5ec89645241e12596b032b9b1e2fd39baf
Satyam Seth <satyam1998.1998@gmail.com>
2026-08-25 00:54:35 +0530

    Refresh Token

---
+++ Geekyshows/Section-27.Authentication and Authorization/6.Refresh Token/106/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.db.config import create_tables
+from app.account.routers import router as account_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(account_router)
